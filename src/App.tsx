7963ca3f5041a7da3df6b4e7b82f2df3d97a52e7
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-02-15 11:17:08 +0200

    add model file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
+
 import ToDoList from "./Components/ToDoList";
 import NewToDo from "./Components/NewToDo";
+import { Todo } from "./todo.model";
 
 import "./App.css";
 
 const App: React.FC = () => {
-  const todos = [
-    { id: 1, text: "Finish TS project and course" },
-    { id: 2, text: "Feed cat" },
-  ];
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: "1", text: "Finish TS project and course" },
+    { id: "2", text: "Feed cat" },
+  ]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([...todos, { id: Math.random().toString(), text: text }]);
   };
 
   return (
--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface ToDoListProps {
   todos: {
-    id: number;
+    id: string;
     text: string;
   }[];
 }
